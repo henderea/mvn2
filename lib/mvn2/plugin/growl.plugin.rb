4b7463e824a1124f0d06fa5300ef604f1ef4730a
Eric Henderson <henderea@gmail.com>
2014-03-21 10:25:10 -0400

    change growl notification to say folder name instead of command

--- lib/mvn2/plugin/growl.plugin.rb
+++ lib/mvn2/plugin/growl.plugin.rb
@@ -7,8 +7,10 @@ class GrowlPlugin
   register :option, sym: :no_sticky, names: %w(-n --no-sticky), desc: 'make the growl notification non-sticky'
 
   register(:notification, order: 100) { |options, _, cmd_clean, message_text|
+    path = Dir.getwd
+    path = path[(path.rindex('/')+1)..-1]
     begin
-      `growlnotify -n Terminal -a Terminal#{(options[:no_sticky] ? '' : ' -s')} -m "#{message_text}" -t "#{cmd_clean}" 2>&1`
+      `growlnotify -n Terminal -a Terminal#{(options[:no_sticky] ? '' : ' -s')} -m "#{message_text}" -t "#{path}" 2>&1`
     end until $?.success?
   }
 end
